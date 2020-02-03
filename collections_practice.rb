82c74b2985ce1f3d06bd4938ed8884be1a466573
Jenise Ruehle <jela07@gmail.com>
2020-02-03 16:04:02 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -37,6 +37,10 @@ def sum_array(array)
 end 
 
 def add_s(array)
-  array.each_with_index.collect {|element, index| 
-
+  array.each_with_index.collect do |element, index| if index == 1  
+      element
+    else   
+      element << "s"
+    end 
+  end  
 end 
